3a9d1ea2f8f03fd7baab0ca1767e62d12a6ab4eb
sudheertangudu <155940847+sudheertangudu@users.noreply.github.com>
2024-01-08 15:57:07 +0530

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+import streamlit
+streamlit.title('My Mom's New Healthy Dinner')
+
+
+streamlit.header('Breakfast Favorates')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
