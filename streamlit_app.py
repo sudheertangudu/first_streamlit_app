031ee017c48e092a53a6ebebc7624be6f255fa06
sudheertangudu <155940847+sudheertangudu@users.noreply.github.com>
2024-01-08 15:36:05 +0530

    Update streamlit_app.py
    New Code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-import streamlit
-
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.text('Please execute this')
-streamlit.header('Okay good fine')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
