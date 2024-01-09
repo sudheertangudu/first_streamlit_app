e644b720beb1a801c7a1dfc652655feb4a0165b2
sudheertangudu <155940847+sudheertangudu@users.noreply.github.com>
2024-01-09 13:24:06 +0530

    Update streamlit_app.py
    Fixed error in function

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ streamlit.dataframe(fruits_to_show)
 # create the repeatable code block (calling function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     return fruityvice_normalized
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 #New section to display fruityvice api responce
 streamlit.header("Fruityvice Fruit Advice!")
